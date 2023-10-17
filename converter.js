786835a55794944f150a023be1b34b50258863e4
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-10-17 07:51:14 +0500

    ready

---
+++ converter.js
@@ -0,0 +1,11 @@
+let marla = document.getElementById("marla");
+let feet = document.getElementById("feet");
+
+function marlaToFt() {
+  let output = (parseFloat(marla.value) * 272.251);
+  feet.value = parseFloat(output.toFixed(3));
+}
+function ftToMarla() {
+  let output = ((parseFloat(feet.value) * 0.00367309));
+  marla.value = parseFloat(output.toFixed(3));
+}
